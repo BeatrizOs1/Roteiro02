d072b059bfc01a393c03987ff1e62d3fc8e0518c
BeatrizOs1 <76188108+BeatrizOs1@users.noreply.github.com>
2024-05-26 22:03:12 -0300

    Update TaskControllerIntegrationTest.java

--- roteiro1-main/roteiro1-main/src/test/java/com.labdesoft.roteiro01/integration/TaskControllerIntegrationTest.java
+++ roteiro1-main/roteiro1-main/src/test/java/com.labdesoft.roteiro01/integration/TaskControllerIntegrationTest.java
@@ -1,17 +1,17 @@
-package com.labdessoft.roteiro01.integration;
+package com.labdesoft.roteiro01.integration;
 
+import com.labdesoft.roteiro01.Roteiro01Application;
+import com.labdesoft.roteiro01.repository.TaskRepository;
 
-import com.labdessoft.roteiro01.Roteiro01Application;
 import io.restassured.RestAssured;
-import org.junit.Before;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-
 
 import static io.restassured.RestAssured.get;
 import static org.hamcrest.Matchers.equalTo;
@@ -22,10 +22,14 @@ import static org.hamcrest.Matchers.equalTo;
 @ActiveProfiles("test")
 public class TaskControllerIntegrationTest {
 
+    @Mock
+    private TaskRepository taskRepository;
+
     @BeforeEach
     public void setup() {
-        RestAssured.baseURI = "https://todolist-7c6q.onrender.com";
-        RestAssured.port = 443; 
+        RestAssured.baseURI = "http://localhost:8080";
+        RestAssured.port = 8080;
+
     }
 
     @Test
@@ -37,5 +41,21 @@ public class TaskControllerIntegrationTest {
     public void givenUrl_whenSuccessOnGetsResponseAndJsonHasOneTask_thenCorrect() {
         get("/api/task/1").then().statusCode(200)
                 .assertThat().body("description", equalTo("Primeira tarefa"));
+    }
+
+
+    @Test
+    public void givenTaskId_whenDeleteTask_thenCorrect() {
+        RestAssured.delete("/api/task/1")
+                .then()
+                .statusCode(204);
+    }
+
+    @Test
+    public void givenTaskId_whenPutCompleteTask_thenCorrect() {
+        RestAssured.put("/api/task/1/complete")
+                .then()
+                .statusCode(200)
+                .assertThat().body("completed", equalTo(true));
     }
 }
